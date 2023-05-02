bc6003d721a4f51318680393423e86afc0aca8f6
김민석 <kmin9811@naver.com>
2023-05-02 09:20:41 +0900

    🚀 Feature - Member, Board Controller 완성

--- src/main/java/com/ssafy/board/controller/BoardController.java
+++ src/main/java/com/ssafy/board/controller/BoardController.java
@@ -1,34 +1,21 @@
 package com.ssafy.board.controller;
 
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.board.model.dto.BoardDto;
 import com.ssafy.board.model.service.BoardService;
-import com.ssafy.board.model.service.BoardServiceImp;
 
 @RestController
 @RequestMapping("/board")
@@ -47,9 +34,9 @@ public class BoardController {
 	}
 	
 	@PostMapping("/write")
-	public ResponseEntity<?> write(HttpSession session, String subject, String content) throws Exception {
+	public ResponseEntity<?> write(@RequestBody BoardDto boardDto, HttpSession session) throws Exception {
 		String id = (String) session.getAttribute("loginUser");
-		boardService.write(id, subject, content);
+		boardService.write(id, boardDto.getSubject(), boardDto.getContent());
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
@@ -60,8 +47,10 @@ public class BoardController {
 	}
 	
 	@PutMapping("/list/{articleNo}")
-	public ResponseEntity<?> modify(@PathVariable("articleNo") String articleNo, String subject, String content) throws Exception {
-		boardService.modify(Integer.valueOf(articleNo), subject, content);
+	public ResponseEntity<?> modify(@PathVariable("articleNo") @RequestBody BoardDto boardDto) throws Exception {
+		
+		// TODO 추후에 modify BoardDto 이용으로 변경
+		boardService.modify(boardDto.getArticleNo(), boardDto.getSubject(), boardDto.getContent());
 		
 		// TODO 바뀐 데이터 보내줘야 함
 		return new ResponseEntity<Void>(HttpStatus.OK); 
@@ -73,159 +62,5 @@ public class BoardController {
 		boardService.delete(Integer.valueOf(articleNo));
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
-	
-	
-	
-//	private void forward(HttpServletRequest request, HttpServletResponse response, String path)
-//			throws ServletException, IOException {
-//		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-//		dispatcher.forward(request, response);
-//	}
-
-//	private void redirect(HttpServletRequest request, HttpServletResponse response, String path) throws IOException {
-//		response.sendRedirect(request.getContextPath() + path);
-//	}
-	
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		request.setCharacterEncoding("utf-8");
-//		doGet(request, response);
-//	}
-	
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		String path = "";
-//		try {
-//			String action = request.getParameter("action");
-//			System.out.println(action);
-//			if ("mvwrite".equals(action)) {
-//				path = "/board/mvwritearticle.jsp";
-//				redirect(request, response, path);
-//			} else if ("write".equals(action)) {
-//				path = write(request, response);
-//				forward(request, response, path);
-//			} else if ("list".equals(action)) {
-//				path = list(request, response); 
-//				forward(request, response, path);
-//			} else if ("view".equals(action)) {
-//				path = view(request, response); 
-//				forward(request, response, path);
-//			} else if ("mvmodify".equals(action)) {
-//				path = mvmodify(request, response);
-//				forward(request, response, path);
-//			} else if ("modify".equals(action)) {
-//				path = modify(request, response);
-//				forward(request, response, path);
-//			} else if ("delete".equals(action)) {
-//				path = delete(request, response);
-//				redirect(request, response, path);
-//			} 
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			path = "/error/error.jsp";
-//			redirect(request, response, path);
-//		}
-//	}
-	
-	/**
-	 * 특정 게시글 페이지에서 글삭제 버튼을 눌렀을 때 동작
-	 * DB에서 articleNo의 article 삭제 
-	 * 
-	 * hidden으로 articleNo가 필요
-	 * 게시글 리스트로 다시 돌아감
-	 * @throws SQLException 
-	 */
-//	private String delete(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		int articleNo = Integer.valueOf(request.getParameter("articleNo"));
-//		
-//		boardService.delete(articleNo);
-//		
-//		return "/board?action=list";
-//	}
-	
-
-	/**
-	 * 게시글 수정 페이지에서 글수정 버튼을 눌렀을 때 동작
-	 * DB에서 게시글의 내용을 수정 
-	 * 
-	 * hidden으로 articleNo가 필요
-	 * 게시글 보기로 다시 돌아감
-	 * @throws SQLException 
-	 */
-//	private String modify(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		int articleNo = Integer.valueOf(request.getParameter("articleNo"));
-//		String subject = request.getParameter("subject");
-//		String content = request.getParameter("content");
-//		
-//		boardService.modify(articleNo, subject, content);
-//		
-//		return "/board?action=view&articleNo=" + articleNo;
-//	}
-
-
-	
-	/**
-	 * 게시판에서 특정 글에서 수정 버튼을 눌렀을 때 동작
-	 * hidden을 통해서 글 번호(articleNo)가 필요 
-	 * 게시글 수정 페이지로 이동
-	 * @throws SQLException 
-	 */
-//	private String mvmodify(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		int articleNo = Integer.valueOf(request.getParameter("articleNo"));
-//		
-//		BoardDto board = boardService.mvmodify(articleNo);		
-//		request.setAttribute("article", board);		
-//		
-//		return "/board/modify.jsp";
-//	}
-
-	/**
-	 * 게시판에서 특정 글을 클릭했을 때 동작
-	 * hidden을 통해서 글 번호(articleNo)가 필요 
-	 * @throws SQLException 
-	 */
-//	private String view(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		int articleNo = Integer.valueOf(request.getParameter("articleNo"));
-//		
-//		BoardDto board = boardService.view(articleNo);		
-//		request.setAttribute("article", board);
-//		
-//		return "/board/view.jsp";
-//	}
-
-
-	
-	/**
-	 * 게시판 글 목록을 조회했을 때 동작
-	 * Board 테이블에 있는 data를 return
-	 * @throws SQLException 
-	 */
-//	private String list(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		List<BoardDto> boardList = boardService.list();
-//		request.setAttribute("boardList", boardList);
-//		
-//		return "/board/list.jsp";
-//	}
-	
-
-
-	/**
-	 * 게시판에서 글작성 버튼을 눌렀을 때 동작
-	 * DB에 글의 내용을 저장하고
-	 * 등록 결과로 이동 
-	 * 
-	 * id에 대한 정보는 session으로 가져올 예정 (loginUser)
-	 * @throws SQLException 
-	 */
-//	private String write(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-//		String id = (String) request.getSession().getAttribute("loginUser");
-//		String subject = request.getParameter("subject");
-//		String content = request.getParameter("content");
-//		System.out.println("!!!!!");
-//		boardService.write(id, subject, content);
-//		
-//		return "/board?action=list";
-//	}
-	
-
 	
 }
--- src/main/java/com/ssafy/member/controller/MemberController.java
+++ src/main/java/com/ssafy/member/controller/MemberController.java
@@ -1,14 +1,6 @@
 package com.ssafy.member.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -32,7 +24,7 @@ public class MemberController {
 		this.memberService = memberService;
 	}
 
-	@PostMapping("/register")
+	@PostMapping("/regist")
 	public ResponseEntity<?> regist(@RequestBody MemberDto memberDto) throws Exception {
 //		memberService.registerMember(memberDto);
 		return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
@@ -52,152 +44,14 @@ public class MemberController {
 	}
 	
 	@PostMapping("/modify")
-	// TODO 앞의 인자들 DTO로 변경하던지 할 것
-	public ResponseEntity<?> modify(String mid, String mname, String mpwd,HttpSession session) throws Exception {
+	public ResponseEntity<?> modify(@RequestBody MemberDto modifyDto, HttpSession session) throws Exception {
 		MemberDto memberDto = (MemberDto)session.getAttribute("userinfo");
-		memberDto.setUserId(mid);
-		memberDto.setUserName(mname);
-		memberDto.setUserPwd(mpwd);
+		memberDto.setUserId(modifyDto.getUserId());
+		memberDto.setUserName(modifyDto.getUserName());
+		memberDto.setUserPwd(modifyDto.getUserPwd());
 		memberService.modifyMember(memberDto);
 		session.setAttribute("userinfo", memberDto);
 		
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
-	
-	
-//	private String register(HttpServletRequest request, HttpServletResponse response) {
-//		MemberDto memberDto = new MemberDto();
-//		memberDto.setUserId(request.getParameter("rid"));
-//		memberDto.setUserName(request.getParameter("rname"));
-//		memberDto.setUserEmail(request.getParameter("remail1")+request.getParameter("remail2"));
-//		memberDto.setUserPwd(request.getParameter("rpwd"));
-//		memberDto.setUserSido(Integer.valueOf(request.getParameter("rsido")));
-//		memberDto.setUserGugun(Integer.valueOf(request.getParameter("rgugun")));
-//		
-//		try {
-//			memberService.registerMember(memberDto);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return "/index.jsp";
-//	}
-	
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		String action = request.getParameter("action");
-//		String path = "";
-//		
-//		if ("register".equals(action)) {
-////			path = register(request, response);
-//			redirect(request, response, path);
-//		} else if ("idcheck".equals(action)) {
-//			int cnt = idcheck(request, response);
-//		} else if ("mvlogin".equals(action)) {
-//			path = "/user/login.jsp";
-//			redirect(request, response, path);
-//		} else if("login".equals(action)) {
-//			path = login(request, response);
-//			redirect(request, response, path);	
-//		} else if("logout".equals(action)) {
-//			path = logout(request, response);
-//			redirect(request, response, path);
-//		} else if("modify".equals(action)) {
-//			path = modify(request, response);
-//			redirect(request, response, path);
-//		} 
-//	}
-//
-//	private String modify(HttpServletRequest request, HttpServletResponse response) {
-//		HttpSession session = request.getSession();
-//		MemberDto memberDto = (MemberDto)session.getAttribute("userinfo");
-//		memberDto.setUserId(request.getParameter("mid"));
-//		memberDto.setUserName(request.getParameter("mname"));
-//		memberDto.setUserPwd(request.getParameter("mpwd"));
-//
-//
-//		try {
-//			memberService.modifyMember(memberDto);
-//			session.setAttribute("userinfo", memberDto);
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//
-//		return "/index.jsp";
-//
-//	}
-//	private String logout(HttpServletRequest request, HttpServletResponse response) {
-//		HttpSession session = request.getSession();
-////		session.removeAttribute("userinfo");
-//		session.invalidate();
-//		return "/index.jsp";
-//	}
-//	private String login(HttpServletRequest request, HttpServletResponse response) {
-//		MemberDto memberDto = new MemberDto();
-//		memberDto.setUserId(request.getParameter("lid"));
-//		memberDto.setUserPwd(request.getParameter("lpwd"));
-//		
-//		System.out.println(memberDto);
-//		
-//		try {
-//			MemberDto returnMemberDto = null;
-//			returnMemberDto = memberService.loginMember(memberDto);
-//			if(returnMemberDto != null) {
-////				session 설정
-//				HttpSession session = request.getSession();
-//				session.setAttribute("userinfo", returnMemberDto);
-//				
-////				cookie 설정
-//				String idsave = request.getParameter("saveid");
-//				if("ok".equals(idsave)) { //아이디 저장을 체크 했다면.
-//					Cookie cookie = new Cookie("ssafy_id", returnMemberDto.getUserId());
-//					cookie.setPath(request.getContextPath());
-////					크롬의 경우 400일이 최대
-////					https://developer.chrome.com/blog/cookie-max-age-expires/
-//					cookie.setMaxAge(60 * 60 * 24 * 365 * 40); //40년간 저장.
-//					response.addCookie(cookie);
-//				} else { //아이디 저장을 해제 했다면.
-//					Cookie cookies[] = request.getCookies();
-//					if(cookies != null) {
-//						for(Cookie cookie : cookies) {
-//							if("ssafy_id".equals(cookie.getName())) {
-//								cookie.setMaxAge(0);
-//								response.addCookie(cookie);
-//								break;
-//							}
-//						}
-//					}
-//				}
-//				return "/index.jsp";
-//			}
-//			else {
-//				request.setAttribute("msg", "아이디 또는 비밀번호 확인 후 다시 로그인하세요.");
-//				return "/index.jsp";
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			request.setAttribute("msg", "로그인 중 에러 발생!!!");
-//			return "/index.jsp";
-//		}
-//	}
-//	private int idcheck(HttpServletRequest request, HttpServletResponse response) {
-//		
-//		return 1;
-//	}
-//
-//
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		request.setCharacterEncoding("utf-8");
-//		doGet(request, response);
-//	}
-//	
-//	private void forward(HttpServletRequest request, HttpServletResponse response, String path)
-//			throws ServletException, IOException {
-//		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-//		dispatcher.forward(request, response);
-//	}
-//	
-//	private void redirect(HttpServletRequest request, HttpServletResponse response, String path) throws IOException {
-//		response.sendRedirect(request.getContextPath() + path);
-//	}
-
 }
