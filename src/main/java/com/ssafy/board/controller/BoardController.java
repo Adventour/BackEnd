9f73c92616045a91a034221c55d736c79f44a286
오성락 <osr0907@naver.com>
2023-05-02 11:01:23 +0900

    🚀 Feature - BoardMapper 완료

--- src/main/java/com/ssafy/board/controller/BoardController.java
+++ src/main/java/com/ssafy/board/controller/BoardController.java
@@ -30,23 +30,25 @@ public class BoardController {
 	public BoardController(BoardService boardService) {
 		this.boardService = boardService;
 	}
-	
+
+	@PostMapping("/write")
+	@ApiOperation(value = "게시글 작성", notes = "게시글 작성 요청 API 입니다.")
+	public ResponseEntity<?> write(@RequestBody BoardDto boardDto, HttpSession session) throws Exception {
+		String id = (String) session.getAttribute("loginUser");
+		boardDto.setUserId(id);
+		System.out.println(boardDto);
+		boardService.writeArticle(boardDto);
+//		boardService.write(id, boardDto.getSubject(), boardDto.getContent());
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
 	@GetMapping("/")
 	@ApiOperation(value = "게시글 리스트 보기", notes = "모든 게시글 요청 API 입니다.")
 	public ResponseEntity<?> list() throws Exception {
 		List<BoardDto> boardList = boardService.listArticle();
 		return new ResponseEntity<List<BoardDto>>(boardList, HttpStatus.OK);
 	}
-	
-	@PostMapping("/write")
-	@ApiOperation(value = "게시글 작성", notes = "게시글 작성 요청 API 입니다.")
-	public ResponseEntity<?> write(@RequestBody BoardDto boardDto, HttpSession session) throws Exception {
-//		String id = (String) session.getAttribute("loginUser");
-		boardService.writeArticle(boardDto);
-//		boardService.write(id, boardDto.getSubject(), boardDto.getContent());
-		return new ResponseEntity<Void>(HttpStatus.OK);
-	}
-	
+
 	@GetMapping("list/{articleNo}")
 	@ApiOperation(value = "게시글 보기", notes = "게시글 보기 요청 API 입니다.")
 	public ResponseEntity<?> view(@PathVariable("articleNo") String articleNo) throws Exception {
@@ -54,18 +56,19 @@ public class BoardController {
 //		BoardDto boardDto = boardService.view(Integer.valueOf(articleNo));
 		return new ResponseEntity<BoardDto>(boardDto, HttpStatus.OK);
 	}
-	
+
 	@PutMapping("/list/{articleNo}")
 	@ApiOperation(value = "게시글 수정", notes = "게시글 수정 요청 API 입니다.")
-	public ResponseEntity<?> modify(@PathVariable("articleNo") @RequestBody BoardDto boardDto) throws Exception {
-		
+	public ResponseEntity<?> modify(@PathVariable("articleNo") String articleNo, @RequestBody BoardDto boardDto) throws Exception {
+
 		// TODO 추후에 modify BoardDto 이용으로 변경
+		boardDto.setArticleNo(Integer.valueOf(articleNo));
 		boardService.modifyArticle(boardDto);
 //		boardService.modify(boardDto.getArticleNo(), boardDto.getSubject(), boardDto.getContent());
 		// TODO 바뀐 데이터 보내줘야 함
-		return new ResponseEntity<Void>(HttpStatus.OK); 
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("/list/{articleNo}")
 	@ApiOperation(value = "게시글 삭제", notes = "게시글 삭제 요청 API 입니다.")
 	public ResponseEntity<?> delete(@PathVariable("articleNo") String articleNo) throws Exception {
@@ -74,5 +77,5 @@ public class BoardController {
 //		boardService.delete(Integer.valueOf(articleNo));
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
+
 }
