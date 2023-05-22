99ecce06dc5f45f0cf57b181c6f9011bc126724d
김민석 <kmin9811@naver.com>
2023-05-22 19:56:37 +0900

    🚀[Feature] - MemberResponseDto 생성

--- src/main/java/com/ssafy/member/controller/MemberController.java
+++ src/main/java/com/ssafy/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ssafy.member.controller;
 import javax.servlet.http.HttpSession;
 
 import com.ssafy.auth.dto.TokenResponseDto;
+import com.ssafy.member.model.dto.MemberResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -32,6 +33,14 @@ import io.swagger.annotations.ApiOperation;
 public class MemberController {
 	private final MemberService memberService;
 
+
+	/** @return id, name, email, domain
+	 * */
+	@GetMapping("/")
+	public ResponseEntity<?> myProfile(Authentication auth) throws Exception {
+		MemberDto memberDto = memberService.findByUserId(auth.getName());
+		return new ResponseEntity<>(MemberResponseDto.of(memberDto), HttpStatus.OK);
+	}
 	@PostMapping("/regist")
 	@ApiOperation(value = "회원가입", notes = "회원가입 요청 API 입니다.")
 	public ResponseEntity<?> regist(@RequestBody MemberDto memberDto) throws Exception {
@@ -45,11 +54,7 @@ public class MemberController {
 		return new ResponseEntity<>(memberDto, HttpStatus.OK);
 	}
 
-	@GetMapping("/test")
-	public ResponseEntity<?> test(Authentication auth) {
-		log.debug("로그인 된 사용자 id : " + auth.getName());
-		return new ResponseEntity<>(auth.getName(), HttpStatus.OK);
-	}
+
 
 	@PostMapping("/login")
 	@ApiOperation(value = "로그인", notes = "로그인 요청 API 입니다.")
