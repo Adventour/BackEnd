48f030f16157ddf8aaa2356eaac6e0923ad7389c
김민석 <kmin9811@naver.com>
2023-05-23 17:09:21 +0900

    🚀 feat(plans): PlanDetail 수정 로직 생성 #8

--- src/main/java/com/ssafy/plan/model/service/PlanServiceImp.java
+++ src/main/java/com/ssafy/plan/model/service/PlanServiceImp.java
@@ -5,6 +5,7 @@ import com.ssafy.attraction.model.mapper.AttractionMapper;
 import com.ssafy.member.model.dto.MemberDto;
 import com.ssafy.plan.model.dto.PlanDetailDto;
 import com.ssafy.plan.model.dto.PlanDto;
+import com.ssafy.plan.model.dto.PlanRequestDto;
 import com.ssafy.plan.model.dto.PlanResponseDto;
 import com.ssafy.plan.model.mapper.PlanMapper;
 import lombok.RequiredArgsConstructor;
@@ -30,25 +31,23 @@ public class PlanServiceImp implements PlanService{
         List<PlanDto> planDtoList = planMapper.findPlansByMemberDto(memberDto);
         for (PlanDto planDto : planDtoList) {
             List<PlanDetailDto> planDetailDtoList = planMapper.findPlanDetailsByPlanDto(planDto);
-            log.debug("PlanServiceImpl pDL size : " + planDetailDtoList.size());
             PlanResponseDto planResponseDto = PlanResponseDto.builder()
+                    .planId(planDto.getPlanId())
                     .planName(planDto.getPlanName())
                     .attractionImages(new String[planDetailDtoList.size()])
                     .attractionTitles(new String[planDetailDtoList.size()])
+                    .contentIds(new int[planDetailDtoList.size()])
                     .build();
-            System.out.println("pRD : " + planResponseDto);
-            System.out.println(planDetailDtoList);
             for (PlanDetailDto planDetailDto : planDetailDtoList) {
-//                planResponseDto.addPlanDetail(planDetailDto);
                 int contentId = planDetailDto.getContentId();
                 AttractionDto attractionDto = attractionMapper.findAttractionById(contentId);
                 int idx = planDetailDto.getPlanOrder();
                 planResponseDto.getAttractionImages()[idx-1] = attractionDto.getImage();
                 planResponseDto.getAttractionTitles()[idx-1] = attractionDto.getTitle();
+                planResponseDto.getContentIds()[idx-1] = attractionDto.getContentId();
             }
             planResponseDtoList.add(planResponseDto);
         }
-        log.debug("PlanServiceImpl pRDL : " + planResponseDtoList);
         return planResponseDtoList;
     }
 
@@ -90,5 +89,25 @@ public class PlanServiceImp implements PlanService{
             planDetailDtoList.add(planDetailDto);
         }
         planMapper.addPlanDetails(planDetailDtoList);
+    }
+
+    @Override
+    public void modifyPlanDetails(String userId, PlanRequestDto planRequestDto) {
+        List<PlanDetailDto> planDetailDtoList = new ArrayList<>();
+        int idx = 1;
+        int planId = planRequestDto.getPlanId();
+        for (Integer contentId : planRequestDto.getContentIds()) {
+            PlanDetailDto planDetailDto = PlanDetailDto.builder()
+                    .planOrder(idx++)
+                    .descript("descript 처리 필요")
+                    .contentId(contentId)
+                    .planId(planId)
+                    .build();
+            planDetailDtoList.add(planDetailDto);
+        }
+        planMapper.deletePlanDetails(planRequestDto.getPlanId());
+        planMapper.addPlanDetails(planDetailDtoList);
+//        planMapper.addPlanDetails();
+
     }
 }
